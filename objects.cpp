b3507d982102e19ea3bda8b0242f0c746215d00a
nick-gl <158052522+nick-gl@users.noreply.github.com>
2024-04-24 13:36:45 -0400

    Update objects.cpp

--- objects.cpp
+++ objects.cpp
@@ -1,7 +1,7 @@
 // I neeed to commentt this more and also my keyboard sucks f this keyboard
 
-#ifndef objects.cpp
-#define objects.cpp
+#ifndef circle_H
+#define circle_H
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <cmath>
@@ -14,13 +14,15 @@ float const pi = 3.14;
 
 class Circle
 { private:
-	sf::Vector2f force(0,0);
-	sf::Vector2f aceleration;
+	sf::Vector2f force;
+    sf::Vector2f velocity;
+	sf::Vector2f acceleration;
 	sf::CircleShape shape;
 	sf::Vector2f position;
     float radius;
 public:
  Circle(float radius, sf::Vector2f accel, sf::Vector2f velo, sf::Vector2f position):
+    force(0,0),
     acceleration(accel),
     velocity(velo), position(position),
     radius(radius){
@@ -46,9 +48,10 @@ public:
     bool checkcollosions(const Circle& circle) {
         sf::Vector2f other_position = circle.getPosition();
 
-        float dist = (position.x-other_position.x)(position.x-other_position.x) + (position.y-other_position.y)(position.y-other_position.y);
+        float dist = (position.x - other_position.x) * (position.x - other_position.x) +
+             (position.y - other_position.y) * (position.y - other_position.y);
         dist = std::sqrt(dist);
-        if dist > radius + circle.getradius()
+        if (dist > radius + circle.getradius())
            { return true;}
         else
             {return false;}
@@ -59,7 +62,7 @@ public:
     }
     void setposition(sf::Vector2f new_position) {
 	position = new_position;
-	shape.setposition(position);
+	shape.setPosition(position);
     }
     float getradius() const {
          return radius;
@@ -69,14 +72,14 @@ public:
 
     }
     /* I'll ask dunc for help
-    
+
     void handle_elastic_collosion(Circle circle) {
      // dis sucks
         float mass = pi*density*radius*radius;
 
         float other_mass = pi*density*circle.getradius()*circle.getradius();
         // use kenetic energy to find velocity I hate physics
-        
+
     }
     void handle_inelastic_collsion {
 
@@ -86,11 +89,13 @@ public:
     void Push_constant_force(sf::Vector2f FORCE) {
         force.x += FORCE.x;
 	force.y += FORCE.y;
-    }    
-   
+    }
+
     void setup(sf::RenderWindow& window) const {
      window.draw(shape);
     }
 
-}
+};
 #endif
+
+// g++ objects.cpp -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
