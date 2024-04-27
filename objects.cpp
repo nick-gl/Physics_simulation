530f140d7b129ad9e2eeaed39129fd4f016a2a62
nick-gl <158052522+nick-gl@users.noreply.github.com>
2024-04-26 20:51:34 -0400

    Update objects.cpp

--- objects.cpp
+++ objects.cpp
@@ -17,23 +17,30 @@ class Circle
 	sf::Vector2f force;
     sf::Vector2f velocity;
 	sf::Vector2f acceleration;
-	sf::CircleShape shape;
+    sf::CircleShape shape;
 	sf::Vector2f position;
-    float radius;
+    const float radius;
 public:
- Circle(float radius, sf::Vector2f accel, sf::Vector2f velo, sf::Vector2f position):
+ Circle(float radius, sf::Vector2f velo, sf::Vector2f position):
     force(0,0),
-    acceleration(accel),
+    acceleration(0,0),
     velocity(velo), position(position),
     radius(radius){
     shape.setFillColor(sf::Color::Blue);
     shape.setRadius(radius);
     shape.setPosition(position);
     }
-    void update(float deltaTime) {
+    void update(float deltaTime,sf::RenderWindow& window) {
+        position = shape.getPosition();
+
+        if (check_floor_and_ceiling(window)) {
+            velocity.y *=-.8;
+            acceleration.y = 0;
+        }
+        else {
 	 acceleration.y = force.y / (density * pi * (radius * radius));
         acceleration.x = force.x / (density * pi * (radius * radius));
-
+        }
         sf::Vector2f final_velocity(0, 0);
         final_velocity.y = velocity.y + acceleration.y * deltaTime;
         final_velocity.x = velocity.x + acceleration.x * deltaTime;
@@ -41,7 +48,6 @@ public:
         // Update position using the distance formula
         shape.setPosition(position.x + 0.5f * (velocity.x + final_velocity.x) * deltaTime,
                            position.y + 0.5f * (velocity.y + final_velocity.y) * deltaTime);
-
         set_manual_velo(final_velocity);
     }
     // Imma do this later
@@ -51,10 +57,29 @@ public:
         float dist = (position.x - other_position.x) * (position.x - other_position.x) +
              (position.y - other_position.y) * (position.y - other_position.y);
         dist = std::sqrt(dist);
-        if (dist > radius + circle.getradius())
-           { return true;}
+        if (dist < radius + circle.getradius())
+           { return false;}
         else
-            {return false;}
+            {return true;}
+    }
+    bool check_floor_and_ceiling(sf::RenderWindow& window) {
+        if (shape.getPosition().y<-100) {
+        return false;
+        }
+
+        unsigned int max = window.getSize().y;
+
+        if (max-shape.getPosition().y<radius && velocity.y>0) {
+            shape.setPosition(shape.getPosition().x,max-radius);
+        return true;
+
+        }
+      /*
+        else if (shape.getPosition().y-radius<0 && velocity.y<0)
+        {
+            return true;
+        }  */
+       return false;
     }
     sf::Vector2f getPosition() const {
         return position;
@@ -88,7 +113,7 @@ public:
 
     void Push_constant_force(sf::Vector2f FORCE) {
         force.x += FORCE.x;
-	force.y += FORCE.y;
+	    force.y += FORCE.y;
     }
 
     void setup(sf::RenderWindow& window) const {
@@ -97,5 +122,29 @@ public:
 
 };
 #endif
-
 // g++ objects.cpp -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+int main() {
+sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Animation");
+//Circle(float radius, sf::Vector2f accel, sf::Vector2f velo, sf::Vector2f position):
+Circle circle = Circle(50, sf::Vector2f(0,0), sf::Vector2f(100,100));
+circle.Push_constant_force(sf::Vector2f(0,150));
+circle.setup(window);
+ while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+
+        window.clear();
+        circle.update(.1,window);
+        circle.setup(window);
+        window.display();
+    }
+
+    return 0;
+}
